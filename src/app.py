f682f8b5e382d3fa09c78d7c63f41e68a73edee3
Renan Sies <renansies@gmail.com>
2026-03-27 16:01:09 -0300

    Use format for data

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        'time': datetime.datetime.now().isoformat(),
+        'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname()
     }), 200
 
