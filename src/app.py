90ecc97dd40fb0597c5d866b0e4ec14aa76bb95a
Renan Sies <renansies@gmail.com>
2026-03-30 21:47:33 -0300

    add message and update time in details endpoint

--- src/app.py
+++ src/app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'hostname': socket.gethostname()
+        'time': datetime.datetime.now().strftime('%I:%M:%S %p on %B %d, %Y'),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, human!'
     }), 200
 
 @app.route('/api/v1/healthz')
